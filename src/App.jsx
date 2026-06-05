a151e659cb63768c7fdecadf24fb1699e5642740
Aashish7710 <choudharyaashish613@gmail.com>
2026-06-05 23:30:02 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <>
-      <h1>hii hru</h1>
-    </>
-  )
+
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/clubs" element={<Clubs />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
