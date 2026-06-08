21b3141ed5f8be74f8db970dcd5b4bcf4e57981b
Aashish7710 <choudharyaashish613@gmail.com>
2026-06-08 08:02:01 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   );
 }
